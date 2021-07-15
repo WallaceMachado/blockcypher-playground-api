5de28d826809f53a3234666df70a766c15873f7f
Raphael Conrado <raphael.conrado@parfin.io>
2021-07-15 12:35:26 -0300

    Removing dynamo comments.

--- src/api/wallet/controller.ts
+++ src/api/wallet/controller.ts
@@ -6,30 +6,6 @@ import CreateAddress from "../../dto/address";
 import Address, { IAddress } from "../../model/address";
 import { WalletList } from "../../dto/wallet";
 
-// import Pet, { IPet } from '../models/pet.model';
-// import { IUser } from '../models/user.model';
-
-// interface ICreatePetInput {
-//   owner: IUser['_id'];
-//   name: IPet['name'];
-// }
-
-// async function CreatePet({ owner, name }: ICreatePetInput): Promise<IPet> {
-//   return await Pet.create({
-//     owner,
-//     name
-//   })
-//     .then((data: IPet) => {
-//       return data;
-//     })
-//     .catch((error: Error) => {
-//       throw error;
-//     });
-// }
-
-// export default {
-//   CreatePet
-// };
 export class WalletController {
 
     createAddress = async (req: Request, res: Response) => {
